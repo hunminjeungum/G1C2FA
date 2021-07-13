701c6881bce01bdb520ba03ca9deea97c55ccea6
SYH08 <yihyunsong@outlook.com>
2021-07-14 08:48:39 +0900

    Create PWC.js

---
+++ JS/PWC.js
@@ -0,0 +1,13 @@
+password = prompt("비밀번호");
+
+    if(password==1111){
+
+      alert(':) Confirmed that you are a student with permission to view this site.');
+
+    }else{
+
+      alert(':( You do not have permission to view this site');
+      window.location.href='https://hunminjeungum.github.io/G1C2FA/50rry.html';
+
+
+    }
