cccebb9f543545c2031524b09656336f9850c1df
SYH08 <yihyunsong@outlook.com>
2021-07-13 23:26:26 +0900

    Create PWC.js

---
+++ PWC.js
@@ -0,0 +1,11 @@
+password = prompt("비밀번호");
+
+    if(password==1111){
+
+      alert(':) Confirmed that you are a student with permission to view this site.');
+
+    }else{
+
+      alert(':( You do not have permission to view this site');
+
+    }
