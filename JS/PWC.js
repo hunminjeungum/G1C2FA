c66df934b3aa456c20eb1ed82b2da0679daf05ba
alexlim08 <85393490+alexlim08@users.noreply.github.com>
2021-07-23 02:25:48 +0900

    Update PWC.js

--- JS/PWC.js
+++ JS/PWC.js
@@ -1,6 +1,6 @@
 password = prompt("Student authentication");
 
-    if(password=='조성욱 최고'){
+    if(password=='1-2반 최고'){
 
       alert(':) Confirmed that you are a student with permission to view this site.');
 
